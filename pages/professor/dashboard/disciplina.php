2343cfb8d1fddf1af4779f9a37df1551cdceaf5c
Fernanda Alves <142699965+FernandaAlvesSilva@users.noreply.github.com>
2024-11-04 12:54:46 -0300

    Update disciplina.php

--- pages/professor/dashboard/disciplina.php
+++ pages/professor/dashboard/disciplina.php
@@ -40,7 +40,7 @@ if (!empty($fotoNome)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Alunos</title>
+    <title>Dashboard - Cursos</title>
     
     <!-- CSS -->
     <link rel="stylesheet" href="../../../assets/scss/professor/dashboard/disciplinas.css">
